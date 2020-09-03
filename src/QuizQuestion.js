c9ae34bbeff924054cd79b94a8238feedb33fc62
clichtenwald <70545118+clichtenwald@users.noreply.github.com>
2020-09-03 13:26:05 -0600

    sixth

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,8 +4,15 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
   handleClick(buttonText) {
     if(buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false })
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState({ incorrectAnswer: true })
     }
+  }
+  constructor(props){
+    super(props)
+    this.state = { incorrectAnswer: false }
   }
   render() {
     return (
@@ -20,6 +27,7 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
       </main>
     )
   }
